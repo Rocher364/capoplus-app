df5bfca59b800819aa41fcb5c6eef3cfe23b42d8
Jn batiste Abdias <jnbaptisteabdias35@icloud.com>
2026-09-10 10:05:25 -0400

    Fix DatabaseSeeder user creation logic and prevent 500 error

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,22 +10,21 @@ class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
-        // Kont Admin
-        User::updateOrCreate(
-            ['email' => 'romererocher@gmail.com'],
-            [
+        // Seeder senp pou kreye kont yo si yo pa egziste
+        if (!User::where('email', 'romererocher@gmail.com')->exists()) {
+            User::create([
                 'name' => 'Admin Rocher',
+                'email' => 'romererocher@gmail.com',
                 'password' => Hash::make('toudji@12#'),
-            ]
-        );
+            ]);
+        }
 
-        // Kont Directeur
-        User::updateOrCreate(
-            ['email' => 'steeve@gmail.com'],
-            [
+        if (!User::where('email', 'steeve@gmail.com')->exists()) {
+            User::create([
                 'name' => 'Directeur Steeve',
+                'email' => 'steeve@gmail.com',
                 'password' => Hash::make('toudji1234'),
-            ]
-        );
+            ]);
+        }
     }
 }
