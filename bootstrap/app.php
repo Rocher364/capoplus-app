3d812fd503114416a6348f37313d328248b12670
Jn batiste Abdias <jnbaptisteabdias35@icloud.com>
2026-09-10 10:30:09 -0400

    Register redirect.auditeur middleware alias in bootstrap/app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'redirect.auditeur' => \App\Http\Middleware\RedirectIfNotAuditeur::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
