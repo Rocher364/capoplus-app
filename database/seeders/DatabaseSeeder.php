4a0220f4d28b90a6b839a06a76ae73ccf0346b06
Jn batiste Abdias <jnbaptisteabdias35@icloud.com>
2026-09-10 09:31:47 -0400

    Update DatabaseSeeder with Admin and Directeur accounts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,23 +3,29 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
+        // Kont Admin
+        User::updateOrCreate(
+            ['email' => 'romererocher@gmail.com'],
+            [
+                'name' => 'Admin Rocher',
+                'password' => Hash::make('toudji@12#'),
+            ]
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Kont Directeur
+        User::updateOrCreate(
+            ['email' => 'steeve@gmail.com'],
+            [
+                'name' => 'Directeur Steeve',
+                'password' => Hash::make('toudji1234'),
+            ]
+        );
     }
 }
