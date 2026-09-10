01f92ee5b830c085042c0f76a2c13c6bee0776a1
Jn batiste Abdias <jnbaptisteabdias35@icloud.com>
2026-09-10 08:50:12 -0400

    Force HTTPS ak TrustProxies pou regle fòm san sekirite nan Render

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -11,15 +10,9 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->append(\App\Http\Middleware\SecurityHeaders::class);
-        $middleware->alias([
-            'redirect.auditeur' => \App\Http\Middleware\RedirectIfAuditeur::class,
-            'ensure.auditeur' => \App\Http\Middleware\EnsureAuditeur::class,
-        ]);
+    ->withMiddleware(function (Middleware ) {
+        ->trustProxies(at: '*');
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderJsonWhen(
-            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
-        );
+    ->withExceptions(function (Exceptions ) {
+        //
     })->create();
