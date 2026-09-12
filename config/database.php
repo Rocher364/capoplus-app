8ead063ce7d97a915edb214193c2057b770a1dbe
Jn batiste Abdias <jnbaptisteabdias35@icloud.com>
2026-09-12 13:06:57 -0400

    Remove .env from repository and set default DB connection to pgsql

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -183,6 +183,7 @@ return [
     ],
 
 ];
+
 
 
 
