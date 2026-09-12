b10b537edb851690f0b0fc0ec5b3b5e05164508f
Jn batiste Abdias <jnbaptisteabdias35@icloud.com>
2026-09-12 12:12:53 -0400

    Fix line endings to LF and add executable permissions to entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 set -e
 
-# Efase ansyen cache pou Laravel ka li DATABASE_URL san konfizyon
 php artisan config:clear
 php artisan cache:clear
 
-# Ekzekite migrasyon ak seeder yo sou PostgreSQL
 php artisan migrate --force
 php artisan db:seed --force
 
-# Demare aplikasyon an
-if [ -f /usr/sbin/apache22 ] || [ -f /usr/sbin/apache2 ]; then
-    exec apache2-foreground
-else
-    exec php artisan serve --host=0.0.0.0 --port=10000
-fi
+exec php artisan serve --host=0.0.0.0 --port=10000
