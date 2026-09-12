a6b5a88ab8cc29e24cc79c14134e748052e279ec
Jn batiste Abdias <jnbaptisteabdias35@icloud.com>
2026-09-12 11:42:45 -0400

    Add entrypoint script to automatically run migrations and seeders on free instance startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,4 @@
 #!/bin/sh
-
-# Kreye fichiye SQLite la si l pa egziste
-if [ ! -f /var/www/database/database.sqlite ]; then
-    touch /var/www/database/database.sqlite
-    chown www-data:www-data /var/www/database/database.sqlite
-fi
-
-# Asire otorizasyon fichiye ak dosye yo korèk
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache /var/www/database
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache /var/www/database
-
-# Ekzekite migrations ak seeders pou kreye kont yo
 php artisan migrate --force
 php artisan db:seed --force
-
-# Clear cache pou evite tout vye konfigirasyon
-php artisan config:clear
-php artisan route:clear
-php artisan view:clear
-
-# Demare PHP-FPM ak Nginx
-php-fpm -D
-nginx -g 'daemon off;'
+php artisan serve --host=0.0.0.0 --port=10000
