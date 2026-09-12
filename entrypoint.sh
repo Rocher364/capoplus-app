8378136fd95f29fd6e1722f21b92679b39f61860
Jn batiste Abdias <jnbaptisteabdias35@icloud.com>
2026-09-12 12:11:07 -0400

    Fix entrypoint execution script to clear cache before migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 set -e
 
-# Ekzekite migrasyon yo ak seeder sou PostgreSQL
+# Efase ansyen cache pou Laravel ka li DATABASE_URL san konfizyon
+php artisan config:clear
+php artisan cache:clear
+
+# Ekzekite migrasyon ak seeder yo sou PostgreSQL
 php artisan migrate --force
 php artisan db:seed --force
 
-# Laliman cache ak optimisation
-php artisan config:cache
-php artisan route:cache
-
-# Demare s?v? a
-exec apache2-foreground || php artisan serve --host=0.0.0.0 --port=10000
+# Demare aplikasyon an
+if [ -f /usr/sbin/apache22 ] || [ -f /usr/sbin/apache2 ]; then
+    exec apache2-foreground
+else
+    exec php artisan serve --host=0.0.0.0 --port=10000
+fi
